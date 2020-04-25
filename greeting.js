e8bf2ff959d42039f649a2592cb725332c1e7fc7
taulmax <46551990+taulmax@users.noreply.github.com>
2020-04-26 00:49:33 +0900

    Update localStorage possible to save.
    Can save currentUser in localStorage!

--- greeting.js
+++ greeting.js
@@ -11,10 +11,26 @@ function paintGreeting(text) {
     greeting.innerText = `Hello ${text}`;
 }
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS)
     if (currentUser === null) {
-        // No user
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
